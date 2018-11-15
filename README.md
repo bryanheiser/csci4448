069ba1ac85668f91f1998693ed8eab2abe6cd6b7
bryanheiser <bryan.heiser@colorado.edu>
2018-11-15 09:11:10 -0700

    part4

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is my repository for CSCI 4448 that includes my semester project.
 
-Imprtant: My audio was not working for Jing, so I have a text file called text\_for\_video.txt that has what I was going to talk about.
+IMPORTANT: Again, my audio was not working for Jing even with an external mic, so I have a text file called text\_for\_video.txt that has what I was going to talk about.
 
 My project code lies in the project folder and the code is in mysite/mywebsite/views.py, mysite/mywebsite/urls.py, and mysite/mysite/urls.py.
 There is other code in there that exists for the framework, when I downloaded Django.
--- project/mysite/mywebsite/views.py
+++ project/mysite/mywebsite/views.py
@@ -8,65 +8,59 @@ def Home(request):
 	f = open("home_html.txt")
 	for line in f:
 		html_out += line
-	#html_out = '<div style="width: 100vw; height: 100px; background:MidnightBlue"><header style="padding: 20px; text-align: center; font-size: 40px;' 
-	#html_out += 'color: white; font-family: "Helvetica", sans-serif;">Capita</header><h3 style="position: absolute; top: 15px; right: 150px"><a style="color:white; text-decoration:none" href="login/">Log In</a></h3>'	
-	#html_out += '<h3 style="position: absolute; top: 15px; right: 50px"><a style="color:white; text-decoration:none" href="cart/">Your Cart</a></h3>'
-	#html_out += '<h3 style="position: absolute; top: 15px; right: 230px"><a style="color:white; text-decoration:none" href="signup/">Sign Up</a></h3></div>'
-	#html_out += '<body style="background: LightBlue; margin: 0;">'
-	#html_out += '<h1 style="padding: 20px; text-align: center; font-family: "Helvetica", sans-serif;">'
-	#html_out += 'Check Out Our Newest Winter Gear!</h1>'
 	response.write(html_out)
 	return response	
 
 def LogIn(request):
 	response = HttpResponse()
-	html_out = '<div style="width: 100vw; height: 100px; background:MidnightBlue">'		
-	html_out += '<header style="padding: 20px; text-align: center; font-size: 40px;' 
-	html_out += 'color: white; font-family: "Helvetica", sans-serif;">Capita</header>'
-	html_out += '<h3 style="position: absolute; top: 15px; left: 40px"><a style="color:white; text-decoration:none" href="">Home</a></h3></div>'
-	html_out += '<body style="background: LightBlue; margin: 0;">'
-	html_out += '<h3 style="padding: 35px;"></h3>'
-	html_out += '<form style="text-align: center;"><h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
-	html_out += 'Email:</h3>'
-	html_out += '<input type="text" name="email">'
-	html_out += '<h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
-	html_out += 'Password:</h3>'
-	html_out += '<input type="text" name="password"></form>'
-	html_out += '<div style="text-align: center"><button type="button">Log In</button></div>'
+	html_out = ""
+	f = open("login_html.txt")
+	for line in f:
+		html_out += line
 	response.write(html_out)
 	return response
 
 def Cart(response):	
 	response = HttpResponse()
-	html_out = '<div style="width: 100vw; height: 100px; background:MidnightBlue">'		
-	html_out += '<header style="padding: 20px; text-align: center; font-size: 40px;' 
-	html_out += 'color: white; font-family: "Helvetica", sans-serif;">Capita</header>'
-	html_out += '<h3 style="position: absolute; top: 15px; left: 40px"><a style="color:white; text-decoration:none" href="">Home</a></h3></div>'
-	html_out += '<body style="background: LightBlue; margin: 0;">'
-	html_out += '<h1 style="padding: 10px; text-align: left; font-family: "Helvetica", sans-serif;">'
-	html_out += 'Your Cart</h1>'
-	html_out += '<p style="padding: 10px; font-size: 30px">Items</p></body>'
+	html_out = ""
+	f = open("cart_html.txt")
+	for line in f:
+		html_out += line
+	
+	#html_out = '<div style="width: 100vw; height: 100px; background:MidnightBlue">'		
+	#html_out += '<header style="padding: 20px; text-align: center; font-size: 40px;' 
+	#html_out += 'color: white; font-family: "Helvetica", sans-serif;">Capita</header>'
+	#html_out += '<h3 style="position: absolute; top: 15px; left: 40px"><a style="color:white; text-decoration:none" href="">Home</a></h3></div>'
+	#html_out += '<body style="background: LightBlue; margin: 0;">'
+	#html_out += '<h1 style="padding: 10px; text-align: left; font-family: "Helvetica", sans-serif;">'
+	#html_out += 'Your Cart</h1>'
+	#html_out += '<p style="padding: 10px; font-size: 30px">Items</p></body>'
 	response.write(html_out)
 	return response
 
 def SignUp(response):
 	response = HttpResponse()	
-	html_out = '<div style="width: 100vw; height: 100px; background:MidnightBlue">'		
-	html_out += '<header style="padding: 20px; text-align: center; font-size: 40px;' 
-	html_out += 'color: white; font-family: "Helvetica", sans-serif;">Capita</header>'
-	html_out += '<h3 style="position: absolute; top: 15px; left: 40px"><a style="color:white; text-decoration:none" href="">Home</a></h3></div>'
-	html_out += '<body style="background: LightBlue; margin: 0;">'
-	html_out += '<h1 style="padding: 10px; text-align: center; font-family: "Helvetica", sans-serif;">'
-	html_out += 'Sign Up</h1>'
-	html_out += '<form style="text-align: center;"><h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
-	html_out += 'Email:</h3>'
-	html_out += '<input type="text" name="email">'
-	html_out += '<h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
-	html_out += 'Password:</h3>'
-	html_out += '<input type="text" name="password">'
-	html_out += '<h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
-	html_out += 'Re-Enter Password:</h3>'
-	html_out += '<input type="text" name="password1"></form>'
-	html_out += '<div style="text-align: center"><button type="button">Sign Up</button></div>'
+	html_out = ""
+	f = open("signup_html.txt")
+	for line in f:
+		html_out += line
+	
+	#html_out = '<div style="width: 100vw; height: 100px; background:MidnightBlue">'		
+	#html_out += '<header style="padding: 20px; text-align: center; font-size: 40px;' 
+	#html_out += 'color: white; font-family: "Helvetica", sans-serif;">Capita</header>'
+	#html_out += '<h3 style="position: absolute; top: 15px; left: 40px"><a style="color:white; text-decoration:none" href="">Home</a></h3></div>'
+	#html_out += '<body style="background: LightBlue; margin: 0;">'
+	#html_out += '<h1 style="padding: 10px; text-align: center; font-family: "Helvetica", sans-serif;">'
+	#html_out += 'Sign Up</h1>'
+	#html_out += '<form style="text-align: center;"><h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
+	#html_out += 'Email:</h3>'
+	#html_out += '<input type="text" name="email">'
+	#html_out += '<h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
+	#html_out += 'Password:</h3>'
+	#html_out += '<input type="text" name="password">'
+	#html_out += '<h3 style="text-align: center; font-family: "Helvetica", sans-serif;">'
+	#html_out += 'Re-Enter Password:</h3>'
+	#html_out += '<input type="text" name="password1"></form>'
+	#html_out += '<div style="text-align: center"><button type="button">Sign Up</button></div>'
 	response.write(html_out)
 	return response
